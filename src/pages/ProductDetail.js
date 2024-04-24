2f65bf14db355f7036b172e78fb565d8cb0f04cd
Krat Verma <krat.verma@intelliswift.com>
2024-04-24 13:22:46 +0530

    small change

--- src/pages/ProductDetail.js
+++ src/pages/ProductDetail.js
@@ -26,7 +26,6 @@ export function ProductDetail() {
           autoClose: 4000,
           closeOnClick: true,
         });
-        // throw new Error("unable to fetch data ---> ", error);
       }
     }
     fetchData();
